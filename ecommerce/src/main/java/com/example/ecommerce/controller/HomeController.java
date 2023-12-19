9e0dffc14063f8160f0fb08d47f52a6d564bcbb8
QuocDuong16 <nguyenquocduong380@gmail.com>
2023-12-19 21:40:25 +0700

    Revert "update"
    This reverts commit 8c2dbb380cae2b1d3d5bcf0359c13fc33407b53c.

--- ecommerce/src/main/java/com/example/ecommerce/controller/HomeController.java
+++ ecommerce/src/main/java/com/example/ecommerce/controller/HomeController.java
@@ -63,58 +63,39 @@ public class HomeController {
 
 	@PostMapping("/createUser")
 	public String createuser(@ModelAttribute Customer customer, @ModelAttribute Seller seller,
-	                         @RequestParam("role") String role, @RequestParam("password") String password, 
-	                         @RequestParam("phone") String phone,  @RequestParam("address") String address,HttpSession session) {
+			@RequestParam("role") String role, HttpSession session) {
 
-	    
-		  boolean isCustomer = "customer".equals(role);
-		    boolean isSeller = "seller".equals(role);
-		    boolean isValidPhone = isValidPhoneNumber(phone);
-	        boolean isValidAddress = isValidAddress(address);
-		    if (isCustomer) {
-		        // Xử lý cho customer
-		        boolean emailExists = accountService.checkEmail(customer.getEmail());
-		        
+		boolean isCustomer = "customer".equals(role);
+		boolean isSeller = "seller".equals(role);
 
-		        if (emailExists) {
-		            session.setAttribute("msg", "Email Id already exists");
-		        } else if (!isValidPhone) {
-		            session.setAttribute("msg", "Số điện thoại không hợp lệ, điện thoại 10 số và bắt đầu bằng 0");
-		        } else if (!isValidAddress) {
-		            session.setAttribute("msg", "Địa chỉ không hợp lệ, vui lòng địa chỉ không được full number");
-		        } else {
-		            // Gọi service để tạo customer
-		            accountService.createAccount(customer);
-		            session.setAttribute("msg", "Customer created successfully");
-		        }
-		    } else if (isSeller) {
-		        // Xử lý cho seller
-		        boolean emailExists = accountService.checkEmail(seller.getEmail());
+		if (isCustomer) {
+			// Xử lý cho customer
+			boolean emailExists = accountService.checkEmail(customer.getEmail());
 
-		        if (emailExists) {
-		            session.setAttribute("msg", "Email Id already exists");
-		        } else if (!isValidPhone) {
-		            session.setAttribute("msg", "Số điện thoại không hợp lệ, điện thoại 10 số và bắt đầu bằng 0");
-		        } else if (!isValidAddress) {
-		            session.setAttribute("msg", "Địa chỉ không hợp lệ, vui lòng địa chỉ không được full number");
-		        } else {
-		            // Gọi service để tạo seller
-		            accountService.createAccount(seller);
-		            session.setAttribute("msg", "Seller created successfully");
-		        }
-		    } else {
-		        session.setAttribute("msg", "Invalid role");
-		    }
+			if (emailExists) {
+				session.setAttribute("msg", "Email Id already exists");
+			} else {
+				// Gọi service để tạo customer
+				accountService.createAccount(customer);
+				session.setAttribute("msg", "Customer created successfully");
+			}
+		} else if (isSeller) {
+			// Xử lý cho seller
+			boolean emailExists = accountService.checkEmail(seller.getEmail());
 
-	    return "restricted/register";
+			if (emailExists) {
+				session.setAttribute("msg", "Email Id already exists");
+			} else {
+				// Gọi service để tạo seller
+				accountService.createAccount(seller);
+				session.setAttribute("msg", "Seller created successfully");
+			}
+		} else {
+			session.setAttribute("msg", "Invalid role");
+		}
+
+		return "restricted/register";
+
 	}
 
-	private boolean isValidPhoneNumber(String phone) {
-	    // Kiểm tra xem số điện thoại bắt đầu bằng 0 và có đúng 10 số hay không
-	    return phone != null && phone.matches("0\\d{9}");
-	}
-	private boolean isValidAddress(String address) {
-	    // Kiểm tra xem địa chỉ có ít nhất một chữ cái hay không
-	    return address != null && address.matches(".*[a-zA-Z]+.*");
-	}
 }
